6a2a1ea999ba408049767fcde4f6d88253356bfc
Aaron <aaron.whitebird@gmail.com>
2023-07-04 00:02:27 -0400

    added spinner to user page

--- src/features/users/components/Users.tsx
+++ src/features/users/components/Users.tsx
@@ -14,6 +14,7 @@ import { Input } from '@/components/ui/Input';
 import { useStore } from '@/stores/stores';
 import { useNavigate } from 'react-router-dom';
 import { observer } from 'mobx-react-lite';
+import Spinner from '@/components/ui/Spinner';
 
 const USERS_PER_PAGE = 10;
 
@@ -23,7 +24,7 @@ enum UserMenuOptions {
 }
 
 const Users: React.FC = () => {
-  const { fetchUserChannels, channels } = useStore('channelStore');
+  const { fetchUserChannels, channels, isLoading } = useStore('channelStore');
   const [currentPage, setCurrentPage] = useState(1);
   const [searchTerm, setSearchTerm] = useState('');
   const navigate = useNavigate();
@@ -57,43 +58,49 @@ const Users: React.FC = () => {
         onChange={(e) => setSearchTerm(e.target.value)}
         className="p-2 border border-gray-200 rounded mb-4"
       />
-      <div className="grid grid-cols-1 sm:grid-cols-2 md-grid-cols-4 lg:grid-cols-4 gap-4 flex-1 grid-flow-row-dense items-start grid-rows-[max-content_1fr]">
-        {displayedChannels.map((channel) => (
-          <Card
-            key={channel.uuid}
-            className="border p-4 rounded shadow relative h-min cursor-pointer dark:hover:bg-accent hover:bg-accent"
-            onClick={() => handleClickUser(channel.uuid)}
-          >
-            <DropdownMenu>
-              <DropdownMenuTrigger className="absolute top-0 right-4">
-                <button className="mt-2 text-right text-2xl">⋮</button>
-              </DropdownMenuTrigger>
-              <DropdownMenuContent>
-                <DropdownMenuItem onSelect={() => console.log(`Message ${channel.name}`)}>
-                  {UserMenuOptions.PROFILE}
-                </DropdownMenuItem>
-                <DropdownMenuItem
-                  onSelect={() => console.log(`View Profile ${channel.name}s profile`)}
-                >
-                  {UserMenuOptions.MESSAGE}
-                </DropdownMenuItem>
-              </DropdownMenuContent>
-            </DropdownMenu>
-            <CardContent className="flex items-center justify-center p-0 mb-2">
-              <Avatar className="h-32 w-32 ">
-                <AvatarImage src={channel.image} className="h-32 w-32 rounded-md" />
-                <AvatarFallback children="Fallback" className="rounded-md" />
-              </Avatar>
-            </CardContent>
-            <CardFooter className="flex-col p-0">
-              <div className="flex items-center gap-2">
-                <p className="font-semibold">{channel.name}</p>
-                <div className="h-3 w-3 bg-green-500 rounded-full"></div>
-              </div>
-            </CardFooter>
-          </Card>
-        ))}
-      </div>
+      {isLoading ? (
+        <div className="mt-10 flex-1">
+          <Spinner />
+        </div>
+      ) : (
+        <div className="grid grid-cols-1 sm:grid-cols-2 md-grid-cols-4 lg:grid-cols-4 gap-4 flex-1 grid-flow-row-dense items-start grid-rows-[max-content_1fr]">
+          {displayedChannels.map((channel) => (
+            <Card
+              key={channel.uuid}
+              className="border p-4 rounded shadow relative h-min cursor-pointer dark:hover:bg-accent hover:bg-accent"
+              onClick={() => handleClickUser(channel.uuid)}
+            >
+              <DropdownMenu>
+                <DropdownMenuTrigger className="absolute top-0 right-4">
+                  <button className="mt-2 text-right text-2xl">⋮</button>
+                </DropdownMenuTrigger>
+                <DropdownMenuContent>
+                  <DropdownMenuItem onSelect={() => console.log(`Message ${channel.name}`)}>
+                    {UserMenuOptions.PROFILE}
+                  </DropdownMenuItem>
+                  <DropdownMenuItem
+                    onSelect={() => console.log(`View Profile ${channel.name}s profile`)}
+                  >
+                    {UserMenuOptions.MESSAGE}
+                  </DropdownMenuItem>
+                </DropdownMenuContent>
+              </DropdownMenu>
+              <CardContent className="flex items-center justify-center p-0 mb-2">
+                <Avatar className="h-32 w-32 ">
+                  <AvatarImage src={channel.image} className="h-32 w-32 rounded-md" />
+                  <AvatarFallback children="Fallback" className="rounded-md" />
+                </Avatar>
+              </CardContent>
+              <CardFooter className="flex-col p-0">
+                <div className="flex items-center gap-2">
+                  <p className="font-semibold">{channel.name}</p>
+                  <div className="h-3 w-3 bg-green-500 rounded-full"></div>
+                </div>
+              </CardFooter>
+            </Card>
+          ))}
+        </div>
+      )}
       <div className="flex justify-between items-center">
         <Button
           disabled={currentPage === 1}
