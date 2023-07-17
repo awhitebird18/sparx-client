de5bce631bca81d68cf31910185a0b58715eec1b
Aaron <aaron.whitebird@gmail.com>
2023-07-17 14:44:38 -0400

    added popover component to project

--- src/features/search/components/Search.tsx
+++ src/features/search/components/Search.tsx
@@ -1,4 +1,5 @@
 import { Button } from '@/components/ui/Button';
+import { Search as SearchIcon } from 'react-bootstrap-icons';
 
 const Search = () => {
   const onClick = () => {
@@ -6,13 +7,15 @@ const Search = () => {
   };
   return (
     <Button
-      className="w-full border border-border rounded-md px-3 h-8 justify-start flex-grow-3 max-w-xl overflow-hidden"
+      className="w-full border border-border rounded-md px-3 h-8 justify-start flex-grow-3 max-w-xl overflow-hidden relative text-muted-foreground"
       variant="ghost"
       onClick={onClick}
     >
-      <span className="text-ellipsis overflow-hidden whitespace-nowrap">
+      <span className="text-ellipsis overflow-hidden whitespace-nowrap pl-6">
         Search Chat Application
       </span>
+
+      <SearchIcon className="absolute top-auto mt-0.5 left-3" />
     </Button>
   );
 };
