a329064cda5894b86185a76daca7452a48b1c56b
Aaron <aaron.whitebird@gmail.com>
2023-09-02 20:05:28 -0400

    resolved es lint warning

--- src/features/auth/routes/index.tsx
+++ src/features/auth/routes/index.tsx
@@ -1,24 +1,16 @@
-import { Navigate, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 
 import Login from '../components/Login';
 import Register from '../components/Register';
 import VerificationSuccess from '../components/VerificationSuccess';
-import { useStore } from '@/stores/RootStore';
 import { observer } from 'mobx-react-lite';
 
 const AuthRoutes = () => {
-  // const { currentUser } = useStore('userStore');
-
-  // if (currentUser) {
-  //   return <Navigate to="/app" />;
-  // }
-
   return (
     <Routes>
       <Route path="register" element={<Register />} />
       <Route path="login" element={<Login />} />
       <Route path="verification-success" element={<VerificationSuccess />} />
-      {/* <Route path="*" element={<Navigate to="login" replace />} /> */}
     </Routes>
   );
 };
