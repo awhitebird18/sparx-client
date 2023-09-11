f54701ad85a8f7728da5b86c2db6b3fda13f3e4c
Aaron <aaron.whitebird@gmail.com>
2023-09-11 19:24:15 -0400

    added no threads fallback

--- src/features/threads/components/Threads.tsx
+++ src/features/threads/components/Threads.tsx
@@ -7,6 +7,7 @@ import Message from '@/features/messages/components/Message';
 import { Message as MessageType } from '@/features/messages/types';
 import { observer } from 'mobx-react-lite';
 import ChannelIcon from '@/features/channels/components/ChannelIcon';
+import NoThreadsFallback from './NoThreadsFallback';
 
 const Threads = () => {
   const { threads, fetchUserTheadsApi } = useStore('threadStore');
@@ -22,41 +23,45 @@ const Threads = () => {
       <ContentLayout title="Threads" disablePadding>
         <div className="relative flex flex-1 bg-card dark:bg-background rounded-xl pb-5 overflow-auto h-full">
           <div className="flex flex-col flex-1 w-full h-fit space-y-6 my-6 mx-4">
-            {threads.map((t: ThreadType) => {
-              const channel = findChannelByUuid(t.rootMessage.channelId);
+            {threads.length ? (
+              threads.map((t: ThreadType) => {
+                const channel = findChannelByUuid(t.rootMessage.channelId);
 
-              if (!channel) return;
-              return (
-                <div
-                  key={t.rootMessage.uuid}
-                  onClick={() => {
-                    fetchThreadMessagesApi(t.rootMessage);
-                  }}
-                  className="w-full h-min cursor-pointer rounded-lg"
-                >
-                  <div className="mb-4 flex items-center gap-2 text-lg">
-                    <ChannelIcon imageUrl={channel.icon} size={23} />
-                    {channel.name}
-                  </div>
-                  <div className=" bg-background hover:bg-hover p-3 rounded-lg border border-border">
-                    <div className="pointer-events-none">
-                      {/* Parent message */}
-                      <Message message={t.rootMessage} showUser />
+                if (!channel) return;
+                return (
+                  <div
+                    key={t.rootMessage.uuid}
+                    onClick={() => {
+                      fetchThreadMessagesApi(t.rootMessage);
+                    }}
+                    className="w-full h-min cursor-pointer rounded-lg"
+                  >
+                    <div className="mb-4 flex items-center gap-2 text-lg">
+                      <ChannelIcon imageUrl={channel.icon} size={23} />
+                      {channel.name}
+                    </div>
+                    <div className=" bg-background hover:bg-hover p-3 rounded-lg border border-border">
+                      <div className="pointer-events-none">
+                        {/* Parent message */}
+                        <Message message={t.rootMessage} showUser />
 
-                      {/* Show message count if greater than 2 */}
-                      {t.replyCount > 2 ? (
-                        <p className="text-userMedium my-2">{t.replyCount} replies</p>
-                      ) : null}
+                        {/* Show message count if greater than 2 */}
+                        {t.replyCount > 2 ? (
+                          <p className="text-userMedium my-2">{t.replyCount} replies</p>
+                        ) : null}
 
-                      {/* Latest replies */}
-                      {t.latestReplies.map((r: MessageType) => {
-                        return <Message key={r.uuid} message={r} showUser />;
-                      })}
+                        {/* Latest replies */}
+                        {t.latestReplies.map((r: MessageType) => {
+                          return <Message key={r.uuid} message={r} showUser />;
+                        })}
+                      </div>
                     </div>
                   </div>
-                </div>
-              );
-            })}
+                );
+              })
+            ) : (
+              <NoThreadsFallback />
+            )}
           </div>
         </div>
       </ContentLayout>
