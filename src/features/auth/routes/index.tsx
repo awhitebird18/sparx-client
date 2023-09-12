c2b1e94da23fa5dc10e9b446be9e36f63491dad5
Aaron <aaron.whitebird@gmail.com>
2023-09-12 02:34:37 -0400

    removed verification success paghe

--- src/features/auth/routes/index.tsx
+++ src/features/auth/routes/index.tsx
@@ -3,7 +3,6 @@ import { observer } from 'mobx-react-lite';
 
 import Login from '../components/Login';
 import Register from '../components/Register';
-import VerificationSuccess from '../components/VerificationSuccess';
 import ForgotPassword from '../components/ForgotPassword';
 import ChangePassword from '../components/ChangePassword';
 
@@ -14,7 +13,6 @@ const AuthRoutes = () => {
       <Route path="login" element={<Login />} />
       <Route path="forgot-password" element={<ForgotPassword />} />
       <Route path="change-password" element={<ChangePassword />} />
-      <Route path="verification-success" element={<VerificationSuccess />} />
     </Routes>
   );
 };
