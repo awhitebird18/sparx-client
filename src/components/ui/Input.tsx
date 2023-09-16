765236d08f700ad8b6fe72ebe2234cdaa3ab4be3
Aaron <aaron.whitebird@gmail.com>
2023-09-16 15:22:59 -0400

    fixed search input

--- src/components/ui/Input.tsx
+++ src/components/ui/Input.tsx
@@ -10,7 +10,7 @@ const Input = React.forwardRef<HTMLInputElement, InputProps>(
       <input
         type={type}
         className={cn(
-          'flex h-10 w-full rounded-md border border-neutral-background bg-transparent px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral-500 focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-neutral-400 focus-visible:ring-offset-5 disabled:cursor-not-allowed disabled:opacity-50 dark:border-border dark:bg-background dark:ring-offset-gray-500 dark:placeholder:text-gray-500 dark:focus-visible:ring-gray-500',
+          'flex h-10 w-full rounded-md border border-border bg-transparent px-3 py-2 text-base file:border-0 file:bg-transparent file:text-sm file:font-medium placeholder:text-neutral focus-visible:outline-zinc-400 focus-visible:ring-1 focus-visible:ring-offset-4 disabled:cursor-not-allowed disabled:opacity-50 dark:border-border dark:bg-background dark:ring-offset-1 dark:placeholder:text-neutral dark:focus-visible:ring-zinc-400',
           className,
         )}
         ref={ref}
