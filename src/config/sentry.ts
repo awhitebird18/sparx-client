93fe02aaf12f4f73d0ff0775ce7760c8529ce499
Aaron <aaron.whitebird@gmail.com>
2023-09-28 22:32:48 -0400

    troubleshooting sentry setup

--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -5,12 +5,7 @@ if (process.env.NODE_ENV === 'production') {
     dsn: process.env.VITE_SENTRY_DSN,
     environment: process.env.NODE_ENV,
     tracePropagationTargets: ['localhost', 'spa-rx.ca'],
-    integrations: [
-      new Sentry.BrowserTracing({
-        tracePropagationTargets: [/^https:\/\/yourproductionserver\.io\/api/],
-      }),
-      new Sentry.Replay(),
-    ],
+    integrations: [new Sentry.Replay()],
     tracesSampleRate: 0.1,
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1.0,
