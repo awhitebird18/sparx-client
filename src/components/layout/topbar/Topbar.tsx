6773ad77f00721fffd1d765501e3fed90c1c78ab
Aaron <aaron.whitebird@gmail.com>
2023-09-28 22:40:14 -0400

    troubleshooting sentry setup

--- src/components/layout/topbar/Topbar.tsx
+++ src/components/layout/topbar/Topbar.tsx
@@ -5,11 +5,16 @@ import HistoryDropdown from '@/features/history/components/HistoryDropdown';
 import Logo from '@/components/logo/Logo';
 
 const Topbar = () => {
+  function methodfg(): void {
+    throw new Error('Function not implemented.');
+  }
+
   return (
     <div className="h-12 border-b border-border flex items-center justify-between px-6 gap-6 bg-neutral-800 dark:bg-hover">
       <div className="flex gap-1.5 hover:bg-transparent h-12 cursor-pointer items-center pointer-events-none">
         <Logo size={5} />
         <h1 className="font-bold flex-grow-1 whitespace-nowrap text-xl text-white">SPARX</h1>
+        <button onClick={() => methodfg()}>click</button>
       </div>
 
       <div className="flex items-center gap-1 max-w-xl w-full">
--- src/config/sentry.ts
+++ src/config/sentry.ts
@@ -1,12 +1,32 @@
 import * as Sentry from '@sentry/react';
+import React from 'react';
+import {
+  createRoutesFromChildren,
+  matchRoutes,
+  useLocation,
+  useNavigationType,
+} from 'react-router-dom';
 
 if (process.env.NODE_ENV === 'production') {
   Sentry.init({
     dsn: process.env.VITE_SENTRY_DSN,
     environment: process.env.NODE_ENV,
-    tracePropagationTargets: ['localhost', 'spa-rx.ca'],
-    integrations: [new Sentry.Replay()],
-    tracesSampleRate: 0.1,
+    tracePropagationTargets: ['localhost', /^https:\/\/spa-rx\.ca/],
+    integrations: [
+      new Sentry.BrowserTracing({
+        // See docs for support of different versions of variation of react router
+        // https://docs.sentry.io/platforms/javascript/guides/react/configuration/integrations/react-router/
+        routingInstrumentation: Sentry.reactRouterV6Instrumentation(
+          React.useEffect,
+          useLocation,
+          useNavigationType,
+          createRoutesFromChildren,
+          matchRoutes,
+        ),
+      }),
+      new Sentry.Replay(),
+    ],
+    tracesSampleRate: 1,
     replaysSessionSampleRate: 0.1,
     replaysOnErrorSampleRate: 1.0,
   });
