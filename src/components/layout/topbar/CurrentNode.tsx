2cac2959a873ace10f4123245e8b47053e8e5e36
Aaron <aaron.whitebird@gmail.com>
2024-04-07 00:29:31 -0400

    fixed current node issue where list was not re-rendering until refresh

--- src/components/layout/topbar/CurrentNode.tsx
+++ src/components/layout/topbar/CurrentNode.tsx
@@ -12,7 +12,12 @@ import { useStore } from '@/stores/RootStore';
 
 import useHistoryState from '@/hooks/useHistoryState';
 import { useMemo } from 'react';
+import advancedFormat from 'dayjs/plugin/advancedFormat';
+import relativeTime from 'dayjs/plugin/relativeTime';
 import dayjs from 'dayjs';
+
+dayjs.extend(advancedFormat);
+dayjs.extend(relativeTime);
 
 type HistoryItem = {
   nodeId: string;
@@ -29,44 +34,55 @@ const CurrentNode = () => {
 
   const handleNavigate = (node: any) => {
     setCurrentChannelUuid(node.nodeId);
-
     setTitle(node.name);
   };
 
-  // Assuming 'array' is your input array of objects
-  const sortedArray = history.sort((a: HistoryItem, b: HistoryItem) => b.timestamp - a.timestamp);
+  // Sort history by timestamp in descending order
+  const sortedArray = useMemo(
+    () => history.sort((a: HistoryItem, b: HistoryItem) => b.timestamp - a.timestamp),
+    [history],
+  );
 
-  const uniqueNodes: HistoryItem[] = useMemo(() => [], []);
-  const seenNodeIds = new Set();
+  // Compute uniqueNodes based on sortedArray
+  const uniqueNodes = useMemo(() => {
+    const seenNodeIds = new Set();
+    return sortedArray
+      .filter((item: HistoryItem) => {
+        if (!seenNodeIds.has(item.nodeId)) {
+          seenNodeIds.add(item.nodeId);
+          const nodeName = findChannelByUuid(item.nodeId)?.name;
+          if (nodeName) {
+            return true; // Keep this item
+          }
+        }
+        return false; // Exclude this item
+      })
+      .map((item) => ({
+        ...item,
+        name: findChannelByUuid(item.nodeId)?.name,
+      }));
+  }, [sortedArray, findChannelByUuid]);
 
-  sortedArray.forEach((item: HistoryItem) => {
-    if (!seenNodeIds.has(item.nodeId)) {
-      const nodeName = findChannelByUuid(item.nodeId)?.name;
-
-      if (nodeName) {
-        uniqueNodes.push({ ...item, name: nodeName });
-      }
-      seenNodeIds.add(item.nodeId);
-    }
-  });
-
-  const populatedHistory = useMemo(() => {
-    return uniqueNodes.reduce((acc: any, item: any) => {
-      const dateKey = dayjs(item.timestamp).format('YYYY-MM-DD'); // Adjust format as needed
-      if (!acc[dateKey]) {
-        acc[dateKey] = [];
-      }
-      acc[dateKey].push(item);
-      return acc;
-    }, {});
-  }, [uniqueNodes]);
+  // Create the populated history structure
+  const populatedHistory = useMemo(
+    () =>
+      uniqueNodes.reduce((acc: any, item: HistoryItem) => {
+        const dateKey = dayjs(item.timestamp).format('YYYY-MM-DD'); // Adjust format as needed
+        if (!acc[dateKey]) {
+          acc[dateKey] = [];
+        }
+        acc[dateKey].push(item);
+        return acc;
+      }, {}),
+    [uniqueNodes],
+  );
 
   const historyArr = Object.entries(populatedHistory);
 
   return (
     <DropdownMenu>
       <DropdownMenuTrigger asChild>
-        <h3 className="leftSide flex gap-3 items-center text-main overflow-hidden w-min max-w-sm">
+        <h3 className="leftSide flex gap-3 items-center text-main w-min max-w-sm">
           <span className="truncate">{currentChannel?.name}</span>
           <ChevronDown size={12} className="mt-1 flex-shrink-0" />
         </h3>
@@ -80,38 +96,44 @@ const CurrentNode = () => {
             <Clock size={15} />
             Recently visited
           </DropdownMenuLabel>
-          <div className="flex flex-col overflow-auto max-h-96 p-2 py-2">
+          <div className="flex flex-col p-2 py-2 overflow-auto max-h-96">
             {historyArr.length ? (
-              historyArr.map(([date, items]: any) => (
-                <div key={date}>
-                  <div className="card bg-card flex px-4 items-center h-8 text-sm tracking-wide">
-                    {dayjs(date).format('MMMM Ddd')}
-                  </div>
+              historyArr.map(([date, items]: any) => {
+                const historyDate = dayjs(date);
 
-                  <div className="overflow-auto max-h-96">
-                    {items.map((item: any) => {
-                      const timeString = dayjs(item.timestamp).format('h:mm a');
+                const isToday = dayjs().isSame(historyDate, 'date');
 
-                      return (
-                        <DropdownMenuItem
-                          key={item.nodeId}
-                          className="flex items-center gap-12 h-11 px-4 w-full justify-between hover:bg-hover rounded-lg truncate text-secondary text-sm"
-                          onClick={() => handleNavigate(item)}
-                        >
-                          <div className="flex items-center gap-3 ">
-                            <Clock size={15} />
-                            <div className="flex items-center gap-2">
-                              Visited
-                              <span className="text-highlight font-semibold">{item.name}</span>
+                return (
+                  <div key={Math.floor(Math.random())}>
+                    <div className="card bg-card flex px-4 items-center h-8 text-sm tracking-wide">
+                      {historyDate.format('MMMM Do')}
+                    </div>
+
+                    <div>
+                      {items.map((item: any) => {
+                        const itemTimeObj = dayjs(item.timestamp);
+
+                        return (
+                          <DropdownMenuItem
+                            key={Math.random() * 1000}
+                            className="flex items-center gap-12 h-11 px-4 w-full justify-between hover:bg-hover rounded-lg truncate text-secondary text-sm"
+                            onClick={() => handleNavigate(item)}
+                          >
+                            <div className="flex items-center gap-3 ">
+                              <Clock size={15} />
+                              <div className="flex items-center gap-2">
+                                Visited
+                                <span className="text-highlight font-semibold">{item.name}</span>
+                              </div>
                             </div>
-                          </div>
-                          {timeString}
-                        </DropdownMenuItem>
-                      );
-                    })}
+                            {isToday ? itemTimeObj.fromNow() : itemTimeObj.format('HH:MM p')}
+                          </DropdownMenuItem>
+                        );
+                      })}
+                    </div>
                   </div>
-                </div>
-              ))
+                );
+              })
             ) : (
               <div className="px-5 py-3 text-secondary">No recent channels to show.</div>
             )}
--- src/hooks/useHistoryState.tsx
+++ src/hooks/useHistoryState.tsx
@@ -1,4 +1,4 @@
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { useLocation } from 'react-router-dom';
 import { useStore } from '@/stores/RootStore';
 
@@ -10,7 +10,10 @@ interface NavigationHistoryItem {
 
 const useHistoryState = () => {
   const location = useLocation();
-  const { currentChannelId } = useStore('channelStore');
+  const { currentChannelId, findChannelByUuid } = useStore('channelStore');
+  const [version, setVersion] = useState(0);
+
+  let timestamp: number;
 
   useEffect(() => {
     // Retrieve the current navigation history from localStorage
@@ -27,24 +30,33 @@ const useHistoryState = () => {
 
     // Check if the current view is the same as the last one in the history
     // to prevent duplicate entries.
+
+    console.log(
+      currentChannelId,
+      currentChannelId === lastItem?.nodeId,
+      primaryView === lastItem?.primaryView,
+    );
     if (
       !currentChannelId ||
       (currentChannelId === lastItem?.nodeId && primaryView === lastItem?.primaryView)
     ) {
       return;
     }
-
+    timestamp = new Date().getTime();
     // Add the new navigation state to the history
     const newHistoryItem: NavigationHistoryItem = {
-      timestamp: new Date().getTime(),
+      timestamp: timestamp,
       primaryView,
       nodeId: currentChannelId,
     };
+    console.log(findChannelByUuid(currentChannelId)?.name, timestamp);
 
     navigationHistory.push(newHistoryItem);
 
     // Save the updated navigation history back to localStorage
     localStorage.setItem('navigationHistory', JSON.stringify(navigationHistory));
+
+    setVersion((prev) => prev + 1);
   }, [location, currentChannelId]);
 
   // Optionally, if you want to consume the navigation history in your component,
@@ -54,6 +66,7 @@ const useHistoryState = () => {
     return historyJson ? JSON.parse(historyJson) : [];
   };
 
+  console.log(getNavigationHistory());
   return getNavigationHistory();
 };
 
